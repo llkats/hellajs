d939a58fd08e0814da37aa98fea715188609dd77
Pawel Szymczykowski <makenai@gmail.com>
2015-08-17 13:14:30 -0700

    Sorry, llkats. m(_ _)m

---
+++ test/test-util.js
@@ -0,0 +1,7 @@
+module.exports.firstLetters = function( list ) {
+  var firstLetters = [];
+  for (var i=0;i<list.length;i++) {
+    firstLetters.push( list[i][0] )
+  }
+  return firstLetters.join('')
+}
